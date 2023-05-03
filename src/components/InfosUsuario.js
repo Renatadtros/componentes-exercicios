bdbfae0aae1d5b69f6790b33a0f1f832a4b7e71a
Renata Detros <renata.redros@gmail.com>
2023-05-03 16:25:30 -0300

    resolução

---
+++ src/components/InfosUsuario.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function InfosUsuario() {
+return (
+    <p><img className="avatarImg" src="https://img.freepik.com/vetores-premium/avatar-de-uma-mulher-ruiva-retrato-de-uma-jovem-ilustracao-em-vetor-de-um-rosto_217290-223.jpg?w=826"/> Renata D.</p>
+    )
+
+}
+
+export default InfosUsuario;
